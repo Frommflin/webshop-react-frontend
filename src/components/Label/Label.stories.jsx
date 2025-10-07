322f9c0239eb0336ab350b94056d36ca594f6f28
rozabelay <rozaybeyn.belay@gmail.com>
2025-10-07 15:00:26 +0200

    modifiedLabel.stories.jsx

--- src/components/Label/Label.stories.jsx
+++ src/components/Label/Label.stories.jsx
@@ -1,20 +1,43 @@
-import Label from "./Label.atom";
+import React from "react";
+import Label from "./Label";
 
 export default {
   title: "Components/Label",
   component: Label,
-};
-
-export const Default = {
-  args: {
-    text: "Username",
-    htmlFor: "username",
+  argTypes: {
+    text: { control: "text" },
+    htmlFor: { control: "text" },
+    type: { 
+      control: { type: "select" },
+      options: ["text", "email", "tel", "password", "address", "custom"]
+    },
+    className: { control: "text" },
   },
 };
 
-export const PasswordLabel = {
-  args: {
-    text: "Password",
-    htmlFor: "password",
-  },
+const Template = (args) => <Label {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  text: "Full Name",
+  htmlFor: "name",
+};
+
+export const Email = Template.bind({});
+Email.args = {
+  type: "email",
+  htmlFor: "email",
+};
+
+export const Telephone = Template.bind({});
+Telephone.args = {
+  type: "tel",
+  htmlFor: "phone",
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  text: "Email",
+  htmlFor: "email",
+  className: "required",
 };
