25d9ffe4d5dac9905df95cd42551028bcd498fa0
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-07 13:44:53 +0200

    adjusting prop name for project standards

--- src/components/Heading/Heading.atom.jsx
+++ src/components/Heading/Heading.atom.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import styles from "./Heading.module.css";
 
-const Heading = ({ type, text, styles = {} }) => {
+const Heading = ({ type, text, styling = {} }) => {
   switch (type) {
     case "h1":
-      return <h1 style={styles}>{text}</h1>;
+      return <h1 style={styling}>{text}</h1>;
     case "h2":
-      return <h2 style={styles}>{text}</h2>;
+      return <h2 style={styling}>{text}</h2>;
     case "h3":
-      return <h3 style={styles}>{text}</h3>;
+      return <h3 style={styling}>{text}</h3>;
     case "h4":
-      return <h4 style={styles}>{text}</h4>;
+      return <h4 style={styling}>{text}</h4>;
     case "h5":
-      return <h5 style={styles}>{text}</h5>;
+      return <h5 style={styling}>{text}</h5>;
     case "h6":
-      return <h6 style={styles}>{text}</h6>;
+      return <h6 style={styling}>{text}</h6>;
     default:
       return (
         <p>Heading type is missing, expecting prop 'type', accepting h1-h6</p>
