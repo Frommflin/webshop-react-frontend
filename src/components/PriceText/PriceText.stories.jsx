f4de613b41013d68ef2dcc19c9969a02b9c94c72
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-10 10:55:40 +0200

    Restoring komponent causing errors

---
+++ src/components/PriceText/PriceText.stories.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PriceText from "./PriceText.atom";
+
+export default {
+  title: "Components/PriceText",
+  component: PriceText,
+  argTypes: {
+    amount: { control: "number" },
+    currency: {
+      control: { type: "select" },
+      options: ["SEK", "EUR", "USD"],
+    },
+    styling: { control: "object" },
+  },
+};
+
+const Template = (args) => <PriceText {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  amount: 299,
+  currency: "SEK",
+};
+
+export const Euro = Template.bind({});
+Euro.args = {
+  amount: 1299.5,
+  currency: "EUR",
+};
+
+export const USD = Template.bind({});
+USD.args = {
+  amount: 89.99,
+  currency: "USD",
+};
+
+export const Discounted = Template.bind({});
+Discounted.args = {
+  amount: 499,
+  currency: "SEK",
+  styling: { color: "red", textDecoration: "line-through" },
+};
+
+export const LargeGreen = Template.bind({});
+LargeGreen.args = {
+  amount: 999,
+  currency: "SEK",
+  styling: { color: "green", fontSize: "1.5rem" },
+};
