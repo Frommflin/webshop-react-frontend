1617587e2cc3c26457612ab585e1b84894740f3b
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-10 16:12:54 +0200

    creating inputfield component

--- src/components/InputField/InputField.molecule.jsx
+++ src/components/InputField/InputField.molecule.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Label from "../Label/Label.atom";
+import Input from "../Input/Input.atom";
 
-const InputField = () => {
-  return <div>InputField</div>;
+const InputField = ({
+  label,
+  inputId,
+  type,
+  value,
+  placeholder,
+  disabled,
+  onChange,
+}) => {
+  return (
+    <div>
+      <Label text={label} htmlFor={inputId} />
+      <Input
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
+    </div>
+  );
 };
 
 export default InputField;
--- src/components/InputField/InputField.stories.jsx
+++ src/components/InputField/InputField.stories.jsx
@@ -6,3 +6,23 @@ export default {
 };
 
 export const Default = {};
+
+export const TextInput = {
+  args: {
+    label: "Text Input",
+    inputId: "input1",
+    type: "text",
+    placeholder: "This is a placeholder",
+    onChange: (e) => console.log(e.target.value),
+  },
+};
+
+export const PasswordInput = {
+  args: {
+    label: "Password",
+    inputId: "input2",
+    type: "password",
+    placeholder: "Enter password",
+    onChange: (e) => console.log(e.target.value),
+  },
+};
