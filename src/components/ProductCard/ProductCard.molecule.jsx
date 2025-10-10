4d8155f62a760dc18ffcbb0b89791a45f93617fd
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-10 17:38:50 +0200

    creating productcard component

--- src/components/ProductCard/ProductCard.molecule.jsx
+++ src/components/ProductCard/ProductCard.molecule.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./ProductCard.module.css";
+import Heading from "../Heading/Heading.atom";
+import Image from "../Image/Image.atom";
+import PriceText from "../PriceText/PriceText.atom";
+import Paragraph from "../Paragraph/Paragraph.atom";
+import Badge from "../Badge/Badge.atom";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({
+  imgPath,
+  imgAlt,
+  badgeText,
+  badgeVariant,
+  productName,
+  productDescription,
+  price,
+}) => {
+  return (
+    <div className={styles.card}>
+      <Heading type="h3" text={productName} />
+      {badgeText && (
+        <Badge
+          text={badgeText}
+          variant={badgeVariant}
+          classNames={styles.cardBadge}
+        />
+      )}
+      <Image path={imgPath} alt={imgAlt} />
+      <PriceText amount={price} />
+      <Paragraph text={productDescription} />
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/components/ProductCard/ProductCard.stories.jsx
+++ src/components/ProductCard/ProductCard.stories.jsx
@@ -5,4 +5,25 @@ export default {
   component: ProductCard,
 };
 
-export const Default = {};
+export const Default = {
+  args: {
+    imgPath: "https://picsum.photos/id/1/200",
+    imgAlt: "product",
+    productName: "Product",
+    productDescription:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus dolor magni voluptatibus iure?",
+    price: 100,
+  },
+};
+
+export const CardWithBadge = {
+  args: {
+    imgPath: "https://picsum.photos/id/1/200",
+    imgAlt: "product",
+    badgeText: "Rea",
+    productName: "Product",
+    productDescription:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus dolor magni voluptatibus iure?",
+    price: 100,
+  },
+};
