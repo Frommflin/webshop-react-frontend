032e199a698e29c2361c0e62a3a3f838d9844298
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-12 14:05:04 +0200

    adding storybook tests for textarea

--- src/components/atoms/Textarea/Textarea.stories.jsx
+++ src/components/atoms/Textarea/Textarea.stories.jsx
@@ -5,4 +5,22 @@ export default {
   component: Textarea,
 };
 
-export const Default = {};
+export const Default = {
+  args: {
+    name: "textarea1",
+    placeholder: "This is a textarea",
+  },
+};
+
+export const Styled = {
+  args: {
+    name: "textarea2",
+    placeholder: "This is the placeholder",
+    styling: {
+      height: "200px",
+      width: "300px",
+      borderColor: "#0080ff",
+      color: "#0080ff",
+    },
+  },
+};
