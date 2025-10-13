12e16b421752efc768228bf8f481a5824be92053
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-13 14:25:26 +0200

    creating basic product display with flex

--- src/components/organisms/ProductDisplay/ProductDisplay.organism.jsx
+++ src/components/organisms/ProductDisplay/ProductDisplay.organism.jsx
@@ -1,7 +1,26 @@
+import ProductCard from "@/components/molecules/ProductCard/ProductCard.molecule";
 import React from "react";
+import styles from "./ProductDisplay.module.css";
 
-const ProductDisplay = () => {
-  return <div>ProductDisplay</div>;
+const ProductDisplay = ({ products = [] }) => {
+  return (
+    <div className={styles.flex}>
+      {products &&
+        products.map((prod) => {
+          return (
+            <ProductCard
+              key={prod.id}
+              imgPath={prod.img}
+              imgAlt={prod.alt}
+              productName={prod.name}
+              productDescription={prod.desc}
+              price={prod.price}
+              badgeText={prod.special}
+            />
+          );
+        })}
+    </div>
+  );
 };
 
 export default ProductDisplay;
--- src/components/organisms/ProductDisplay/ProductDisplay.stories.jsx
+++ src/components/organisms/ProductDisplay/ProductDisplay.stories.jsx
@@ -5,4 +5,38 @@ export default {
   component: ProductDisplay,
 };
 
-export const Default = {};
+const prods = [
+  {
+    name: "Product 1",
+    id: "prod1",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ad eos commodi necessitatibus.",
+    price: 1199,
+    special: null,
+    img: "https://picsum.photos/id/1/200",
+    alt: "Product 1 image",
+  },
+  {
+    name: "Product 2",
+    id: "prod2",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ad eos commodi necessitatibus.",
+    price: 2000,
+    special: "REA",
+    img: "https://picsum.photos/id/2/200",
+    alt: "Product 2 image",
+  },
+  {
+    name: "Product 3",
+    id: "prod3",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ad eos commodi necessitatibus.",
+    price: 500,
+    special: null,
+    img: "https://picsum.photos/id/3/200",
+    alt: "Product 3 image",
+  },
+];
+
+export const Default = {
+  args: {
+    products: prods,
+  },
+};
