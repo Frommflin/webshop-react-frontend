a2a5c10a479928d2994a52f54dfeac308c79b947
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-13 09:22:18 +0200

    adjusting button to handle disabled attribute

--- src/components/atoms/Button/Button.atom.jsx
+++ src/components/atoms/Button/Button.atom.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ label, onClick, icon, styling = {} }) => {
+const Button = ({ label, onClick, icon, isDisabled = false, styling = {} }) => {
+  const classnames = isDisabled ? styles.disabled : styles.active;
+
   return (
-    <button className={styles.btn} style={styling} onClick={onClick}>
+    <button
+      className={`${styles.btn} ${classnames}`}
+      style={styling}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
       {icon && <span>{icon}</span>}
       {label && <span>{label}</span>}
     </button>
