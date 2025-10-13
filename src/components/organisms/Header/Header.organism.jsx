476febedc01b3f29e19dac4ab538440e62941dfc
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-13 10:53:51 +0200

    creating basic header

--- src/components/organisms/Header/Header.organism.jsx
+++ src/components/organisms/Header/Header.organism.jsx
@@ -1,7 +1,21 @@
+import Heading from "@/components/atoms/Heading/Heading.atom";
+import Link from "@/components/atoms/Link/Link.atom";
 import React from "react";
+import styles from "./Header.module.css";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ title, links }) => {
+  const navlinks = Array.isArray(links)
+    ? links.map((link) => (
+        <Link key={link.path} label={link.pageName} path={link.path} />
+      ))
+    : [];
+
+  return (
+    <header className={styles.header}>
+      <Heading type="h1" text={title} />
+      <nav>{navlinks}</nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/organisms/Header/Header.stories.jsx
+++ src/components/organisms/Header/Header.stories.jsx
@@ -1,8 +1,24 @@
+import { MemoryRouter } from "react-router-dom";
 import Header from "./Header.organism";
 
 export default {
   title: "Components/Organisms/Header",
   component: Header,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
-export const Default = {};
+export const Default = {
+  args: {
+    title: "Header",
+    links: [
+      { pageName: "Page1", path: "/" },
+      { pageName: "Page2", path: "/page2" },
+    ],
+  },
+};
