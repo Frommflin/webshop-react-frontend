bfa2752f66e167b9981f85d2df36bc54314f140d
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-15 10:16:01 +0200

    creating card component for styling purposes

--- src/components/atoms/Card/Card.atom.jsx
+++ src/components/atoms/Card/Card.atom.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Card.module.css";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ children, styling = {} }) => {
+  return (
+    <div className={styles.card} style={styling}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/atoms/Card/Card.stories.jsx
+++ src/components/atoms/Card/Card.stories.jsx
@@ -6,3 +6,24 @@ export default {
 };
 
 export const Default = {};
+
+export const WithContent = () => {
+  const styleOverride = {
+    backgroundColor: "#e6ffdb",
+  };
+  return (
+    <Card styling={styleOverride}>
+      <h1>This is a title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti soluta
+        voluptatum pariatur, deserunt voluptatibus vel cumque accusamus culpa,
+        ipsa et sequi quaerat ad facilis dolorem ea ut, repellendus architecto
+        libero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum
+        sapiente earum pariatur? Explicabo, non voluptate?
+      </p>
+    </Card>
+  );
+};
