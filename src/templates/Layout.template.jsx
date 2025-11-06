0b1111bae741e9e1e16bf8ae0593cccce00779cd
Klara Frommelin <freakyfrommelin@outlook.com>
2025-11-06 10:05:39 +0100

    adding cookiebanner to layout file

--- src/templates/Layout.template.jsx
+++ src/templates/Layout.template.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/molecules/Footer/Footer.molecule";
 import Header from "@/components/organisms/Header/Header.organism";
 import React from "react";
 import styles from "./Layout.module.css";
+import CookieBanner from "@/components/molecules/CookieBanner/CookieBanner.molecule";
 
 const Layout = ({ children }) => {
   const pages = [
@@ -14,10 +15,18 @@ const Layout = ({ children }) => {
     { pageName: "Produkter", path: "/products" },
     { pageName: "Produkt information", path: "/product/1" }, //Remove when product list is built and each product navigates to this page chen clicked
   ];
+  const handleCookieAccept = () => {
+    console.log("Cookies has been accepted");
+    // TODO: Send consent to auth backend
+  };
   return (
     <div className={styles.page}>
       <Header title="WebbShop!" links={pages} />
       <div className={styles.main}>{children}</div>
+      <CookieBanner
+        onClick={handleCookieAccept}
+        styling={{ marginBottom: "4rem" }}
+      />
       <Footer />
     </div>
   );
